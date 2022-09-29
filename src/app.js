346833af5f1f3e70202a311c5f85ec88b2d944cc
UnassignedByte2504 <marcos.asp989@gmail.com>
2022-09-29 09:25:34 +0000

    First Try

--- src/app.js
+++ src/app.js
@@ -1,11 +1,38 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector("#domgen").addEventListener("click", () => {
+    document.querySelector("#domname").innerHTML = dombuilder();
+    document.querySelector("#domname2").innerHTML = dombuilder();
+    document.querySelector("#domname3").innerHTML = dombuilder();
+    document.querySelector("#domname4").innerHTML = dombuilder();
+    document.querySelector("#domname5").innerHTML = dombuilder();
+  });
 };
+
+function dombuilder() {
+  var domid = 0;
+  for (let domid = 0; domid <= 8; domid++) {
+    let ablock = ["the", "your", "some"];
+    let bblock = ["super", "great", "magnificent", "extraordinary"];
+    let cblock = ["book", "shop", "news", "gram"];
+    let dblock = [".com", ".net", " .es", ".gob.eu", ".shop", ".es"];
+
+    let ablockindex = Math.floor(Math.random() * ablock.length);
+    let bblockindex = Math.floor(Math.random() * bblock.length);
+    let cblockindex = Math.floor(Math.random() * cblock.length);
+    let dblockindex = Math.floor(Math.random() * dblock.length);
+
+    return (
+      ablock[ablockindex] +
+      bblock[bblockindex] +
+      cblock[cblockindex] +
+      dblock[dblockindex]
+    );
+  }
+  domid++;
+}
